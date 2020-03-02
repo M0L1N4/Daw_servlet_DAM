ade44ce862c5de99217d436d3064d98cbd4779f2
alvaro.molina <alvaro.molina@iesjoandaustria.org>
2020-03-02 16:04:52 +0100

    AppIndexTest.java: added example.jsp: added

---
+++ src/main/java/daw_servlet_DAM/AppIndexTest.java
@@ -0,0 +1,14 @@
+package daw_servlet_DAM;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppIndexTest {
+
+	@Test
+	public void testDoGet() {
+		assertFalse(AppIndex.resposta());
+	}
+
+}
