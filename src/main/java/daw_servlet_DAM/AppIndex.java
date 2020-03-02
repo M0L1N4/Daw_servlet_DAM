897aff400ac509be62928cef135ff391a4ebec70
alvaro.molina <alvaro.molina@iesjoandaustria.org>
2020-03-02 15:57:00 +0100

    AppIndexTest.java: Test class added example.jsp: file added .classpath: added test folder AppIndex.java: method "resposta" added index.jsp: changed folder LibraryTest.java: changed folder

--- src/main/java/daw_servlet_DAM/AppIndex.java
+++ src/main/java/daw_servlet_DAM/AppIndex.java
@@ -1,6 +1,8 @@
 package daw_servlet_DAM;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,12 +22,37 @@ public class AppIndex extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    private String message;
+    
+    private static boolean resp = false;
+    
+    public void init() throws ServletException {
+       // Do required initialization
+       message = "Hello World";
+    }
+    
+    public static boolean resposta() {
+    	if (resp == false) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
+		resp = true;
+		// Set response content type
+//	      response.setContentType("text/html");
+
+	      // Actual logic goes here.
+//	      PrintWriter out = response.getWriter();
+//	      out.println("<h1>" + message + "</h1>");
 	}
 
 	/**
